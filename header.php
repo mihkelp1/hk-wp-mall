02aef7b45a22d8bbb8603aac7367389624bc09b0
raido357@gmail.com <raido357@gmail.com@d7ce5ed5-6a65-1e91-dfe9-0b47fc94c34d>
2012-06-05 11:34:48 +0000

    HTML5 doctype, title, head, HTML5 JS library for < IE 9, svn:ignore .* in folder js

--- header.php
+++ header.php
@@ -2,12 +2,45 @@
 /**
  * The Header for our theme.
  */
- 
 ?>
-
-<html>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
 <head>
-<title>0.001v</title>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<title><?php
+	/*
+	 * Print the <title> tag based on what is being viewed.
+	 */
+	wp_title( '|', true, 'right' );
+
+	// Add the blog name.
+	bloginfo( 'name' );
+
+	// Add the blog description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) ) {
+		echo " | $site_description";
+	}
+	?></title>
+
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<!--[if lt IE 9]>
+<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+<style type="text/css">
+	input {
+		display:none;
+	}
+</style>
+<![endif]-->
+
+<?php
+	/* Always have wp_head() just before the closing </head>
+	 * tag of your theme, or you will break many plugins, which
+	 * generally use this hook to add elements to <head> such
+	 * as styles, scripts, and meta tags.
+	 */
+	wp_head();
+?>
 </head>
 
 <body>
