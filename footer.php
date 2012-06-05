68926e108174db2751660784d9d25ce82074bed7
raido357@gmail.com <raido357@gmail.com@d7ce5ed5-6a65-1e91-dfe9-0b47fc94c34d>
2012-06-05 13:17:07 +0000

    Add header shadow, header middle, college logo etc. basic header layout is set

--- footer.php
+++ footer.php
@@ -4,21 +4,23 @@
  */
 ?>
 
-	<footer>
-		<nav>
-			<h2>Info tudengile</h2>
-			<?php wp_nav_menu( array( 'theme_location' => 'footer-menu-left', 'container_class' => 'footer-menu-container-left' ) ); ?>
-		</nav>
-		<nav>
-			<h2>Info sisseastujatele</h2>
-			<?php wp_nav_menu( array( 'theme_location' => 'footer-menu-middle', 'container_class' => 'footer-menu-container-middle' ) ); ?>
-		</nav>
-		<nav>
-			<h2>Arendusprojektid</h2>
-			<?php wp_nav_menu( array( 'theme_location' => 'footer-menu-right', 'container_class' => 'footer-menu-container-right' ) ); ?>
-		</nav>
-	</footer>
+		<footer>
+			<nav>
+				<h2>Info tudengile</h2>
+				<?php wp_nav_menu( array( 'theme_location' => 'footer-menu-left', 'container_class' => 'footer-menu-container-left' ) ); ?>
+			</nav>
+			<nav>
+				<h2>Info sisseastujatele</h2>
+				<?php wp_nav_menu( array( 'theme_location' => 'footer-menu-middle', 'container_class' => 'footer-menu-container-middle' ) ); ?>
+			</nav>
+			<nav>
+				<h2>Arendusprojektid</h2>
+				<?php wp_nav_menu( array( 'theme_location' => 'footer-menu-right', 'container_class' => 'footer-menu-container-right' ) ); ?>
+			</nav>
+		</footer>
 	
+	<!-- Close page-content -->
+	</div>
 <!-- Close "main-conteiner" -->
 </div>
 
