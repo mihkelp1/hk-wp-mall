90836e13c0d9a9ae8544397a3011ecf675c66fcd
raido357@gmail.com <raido357@gmail.com@d7ce5ed5-6a65-1e91-dfe9-0b47fc94c34d>
2012-06-05 10:59:31 +0000

    Add theme functions.php file, add 5 menus, header, navigation and 3 footer menus

--- footer.php
+++ footer.php
@@ -4,5 +4,11 @@
  */
 ?>
 
+<?php wp_nav_menu( array( 'theme_location' => 'footer-menu-left', 'container_class' => 'footer-menu-container-left' ) ); ?>
+
+<?php wp_nav_menu( array( 'theme_location' => 'footer-menu-middle', 'container_class' => 'footer-menu-container-middle' ) ); ?>
+
+<?php wp_nav_menu( array( 'theme_location' => 'footer-menu-right', 'container_class' => 'footer-menu-container-right' ) ); ?>
+
 </body>
 </html>
