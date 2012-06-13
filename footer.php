c07108320aec689756b6046268a630ec34794378
raido357@gmail.com <raido357@gmail.com@d7ce5ed5-6a65-1e91-dfe9-0b47fc94c34d>
2012-06-13 09:53:06 +0000

    Footer menus styles

--- footer.php
+++ footer.php
@@ -9,18 +9,20 @@
 
 	<div id="footer-wrapper">
 		<footer>
-			<nav>
-				<h2>Info tudengile</h2>
-				<?php wp_nav_menu( array( 'theme_location' => 'footer-menu-left', 'container_class' => 'footer-menu-container-left' ) ); ?>
-			</nav>
-			<nav>
-				<h2>Info sisseastujatele</h2>
-				<?php wp_nav_menu( array( 'theme_location' => 'footer-menu-middle', 'container_class' => 'footer-menu-container-middle' ) ); ?>
-			</nav>
-			<nav>
-				<h2>Arendusprojektid</h2>
-				<?php wp_nav_menu( array( 'theme_location' => 'footer-menu-right', 'container_class' => 'footer-menu-container-right' ) ); ?>
-			</nav>
+			<div class="footer-menus-wrapper">
+				<nav class="footer-nav-menu">
+					<h2 class="menu-title">Info tudengile</h2>
+					<?php wp_nav_menu( array( 'theme_location' => 'footer-menu-left', 'container_class' => 'footer-menu-container-left' ) ); ?>
+				</nav>
+				<nav class="footer-nav-menu">
+					<h2 class="menu-title">Info sisseastujatele</h2>
+					<?php wp_nav_menu( array( 'theme_location' => 'footer-menu-middle', 'container_class' => 'footer-menu-container-middle' ) ); ?>
+				</nav>
+				<nav class="footer-nav-menu no-border">
+					<h2 class="menu-title">Arendusprojektid</h2>
+					<?php wp_nav_menu( array( 'theme_location' => 'footer-menu-right', 'container_class' => 'footer-menu-container-right' ) ); ?>
+				</nav>
+			</footer>
 		</footer>
 	</div>
 	
