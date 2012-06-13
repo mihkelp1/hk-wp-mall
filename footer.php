dc9d16ce1805af86b67d417c3e61177e4375f59a
raido357@gmail.com <raido357@gmail.com@d7ce5ed5-6a65-1e91-dfe9-0b47fc94c34d>
2012-06-13 11:06:24 +0000

    Add footer sidebar, wp_nav_menu_title helper method etc.

--- footer.php
+++ footer.php
@@ -11,18 +11,25 @@
 		<footer>
 			<div class="footer-menus-wrapper">
 				<nav class="footer-nav-menu">
-					<h2 class="menu-title">Info tudengile</h2>
+					<h2 class="menu-title"><?php echo wp_nav_menu_title( 'footer-menu-left' ); ?></h2>
 					<?php wp_nav_menu( array( 'theme_location' => 'footer-menu-left', 'container_class' => 'footer-menu-container-left' ) ); ?>
 				</nav>
 				<nav class="footer-nav-menu">
-					<h2 class="menu-title">Info sisseastujatele</h2>
+					<h2 class="menu-title"><?php echo wp_nav_menu_title( 'footer-menu-middle' ); ?></h2>
 					<?php wp_nav_menu( array( 'theme_location' => 'footer-menu-middle', 'container_class' => 'footer-menu-container-middle' ) ); ?>
 				</nav>
 				<nav class="footer-nav-menu footer-right-menu no-border">
-					<h2 class="menu-title">Arendusprojektid</h2>
+					<h2 class="menu-title"><?php echo wp_nav_menu_title( 'footer-menu-right' ); ?></h2>
 					<?php wp_nav_menu( array( 'theme_location' => 'footer-menu-right', 'container_class' => 'footer-menu-container-right' ) ); ?>
 				</nav>
-			</footer>
+			</div>
+		<?php
+			/* A sidebar in the footer? Yep. You can can customize
+			 * your footer with three columns of widgets.
+			 */
+			if ( ! is_404() )
+				get_sidebar( 'footer' );
+		?>
 		</footer>
 	</div>
 	
--- functions.php
+++ functions.php
@@ -12,17 +12,25 @@
  *
  */
  
-function register_my_menus() {
-  register_nav_menus(
-    array( 'header-menu' => __( 'Header menu' ),
-    	'nav-menu' => __( 'Navigation menu' ),
-    	'footer-menu-left' => __('Footer left'),
-    	'footer-menu-middle' => __('Footer middle'),
-    	'footer-menu-right' => __('Footer right') )
-  );
+function _initTheme() {
+ 	register_nav_menus(
+		array( 'header-menu' => __( 'Header menu' ),
+			'nav-menu' => __( 'Navigation menu' ),
+			'footer-menu-left' => __('Footer left'),
+			'footer-menu-middle' => __('Footer middle'),
+			'footer-menu-right' => __('Footer right') )
+		 );
+		
+	register_sidebar(array(
+	  'name' => __( 'Footer sidebar' ),
+	  'id' => 'footer-sidebar',
+	  'before_widget' => '<div id="%1$s" class="widget %2$s">',
+	  'after_widget'  => '</div>',
+	  'description' => __( 'Widgets in this area will be shown in the footer.' )
+	));
 }
 
-add_action( 'init', 'register_my_menus' );
+add_action( 'init', '_initTheme' );
 
 /**
  * Helper method for loading images
@@ -32,6 +40,28 @@ add_action( 'init', 'register_my_menus' );
 
 function getFileURL( $file_name ) {
 	return get_template_directory_uri().$file_name;
+}
+
+/**
+ * Get menu title/name
+ *
+ * @return String
+ */
+
+//If function doesn't exist
+if ( !function_exists( 'wp_nav_menu_title' ) ) {
+	function wp_nav_menu_title( $theme_location ) {
+		$title = '';
+		if ( $theme_location && ( $locations = get_nav_menu_locations() ) && isset( $locations[ $theme_location ] ) ) {
+			$menu = wp_get_nav_menu_object( $locations[ $theme_location ] );
+				
+			if( $menu && $menu->name ) {
+				$title = $menu->name;
+			}
+		}
+	
+		return apply_filters( 'wp_nav_menu_title', $title, $theme_location );
+	}
 }
 
 ?>
