13518aca57fad6756e16953eb06ea067e6b98f78
raido357@gmail.com <raido357@gmail.com@d7ce5ed5-6a65-1e91-dfe9-0b47fc94c34d>
2012-07-25 07:18:40 +0000

    Add latin-ext font support

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 	}
 	?></title>
 
-<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700|Ubuntu+Condensed' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700|Ubuntu+Condensed&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
