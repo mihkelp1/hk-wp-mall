2546d96f2b1c038b7cd3bb316b011b1409e613a2
raido357@gmail.com <raido357@gmail.com@d7ce5ed5-6a65-1e91-dfe9-0b47fc94c34d>
2012-07-26 08:38:00 +0000

    Left side menu taking shape

--- content-page.php
+++ content-page.php
@@ -6,40 +6,44 @@
 
 <div style="height: 455px; border: 1px solid yellow;">
 
-<?php
-	$child_of = $post->post_parent ? $post->post_parent : get_the_ID();
-	if ( count( $post->ancestors ) > 1 ) {
-		$child_of = $post->ancestors[1];
-	}
-	$depth = ( get_children( get_the_ID() ) && $post->post_parent ) || count($post->ancestors) > 1 ? 2 : 1;
-	$args = array(
-		'depth'        => $depth,
-		'date_format'  => get_option('date_format'),
-		'child_of'     => $child_of,
-		'title_li'     => '',
-		'echo'         => 1,
-		'sort_column'  => 'menu_order, post_title',
-		'post_type'    => 'page',
-		'post_status'  => 'publish' 
-	);
-	echo '<ul class="parent_pages">';
-	wp_list_pages( $args );
-	echo '</ul>';
-?>
+	<div class="page-left-menu">
+		<?php
+			$child_of = $post->post_parent ? $post->post_parent : get_the_ID();
+			if ( count( $post->ancestors ) > 1 ) {
+				$child_of = $post->ancestors[1];
+			}
+			$depth = ( get_children( get_the_ID() ) && $post->post_parent ) || count($post->ancestors) > 1 ? 2 : 1;
+			$args = array(
+				'depth'        => $depth,
+				'date_format'  => get_option('date_format'),
+				'child_of'     => $child_of,
+				'title_li'     => '',
+				'echo'         => 1,
+				'sort_column'  => 'menu_order, post_title',
+				'post_type'    => 'page',
+				'post_status'  => 'publish' 
+			);
+			echo '<ul class="parent-pages">';
+			wp_list_pages( $args );
+			echo '</ul>';
+		?>
 
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<header class="entry-header">
-			<h1 class="entry-title"><?php the_title(); ?></h1>
-		</header><!-- .entry-header -->
-	
-		<div class="entry-content">
-			<?php the_content(); ?>
-			<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
-		</div><!-- .entry-content -->
-		<footer class="entry-meta">
-			<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
-		</footer><!-- .entry-meta -->
-	</article><!-- #post-<?php the_ID(); ?> -->
+	</div>
+	<div class="page-inside-content">
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<header class="entry-header">
+				<h1 class="entry-title"><?php the_title(); ?></h1>
+			</header><!-- .entry-header -->
+		
+			<div class="entry-content">
+				<?php the_content(); ?>
+				<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
+			</div><!-- .entry-content -->
+			<footer class="entry-meta">
+				<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
+			</footer><!-- .entry-meta -->
+		</article><!-- #post-<?php the_ID(); ?> -->
+	</div>
 	
 </div>
 
