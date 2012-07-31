3897354587743b7ac3a06bfc3dd261f94955e1c6
raido357@gmail.com <raido357@gmail.com@d7ce5ed5-6a65-1e91-dfe9-0b47fc94c34d>
2012-07-31 11:57:41 +0000

    Display only year, date or day in archive header

--- trunk/archive.php
+++ trunk/archive.php
@@ -29,11 +29,11 @@ get_header(); ?>
 				<header class="content-news-header">
 					<h1 class="page-title">
 						<?php if ( is_day() ) : ?>
-							<?php printf( __( 'Daily Archives: %s', 'twentyeleven' ), '<span>' . get_the_date() . '</span>' ); ?>
+							<?php echo '<span>' . get_the_date() . '</span>'; ?>
 						<?php elseif ( is_month() ) : ?>
-							<?php printf( __( 'Monthly Archives: %s', 'twentyeleven' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'twentyeleven' ) ) . '</span>' ); ?>
+							<?php echo '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'twentyeleven' ) ) . '</span>'; ?>
 						<?php elseif ( is_year() ) : ?>
-							<?php printf( __( 'Yearly Archives: %s', 'twentyeleven' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'twentyeleven' ) ) . '</span>' ); ?>
+							<?php  echo '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'twentyeleven' ) ) . '</span>'; ?>
 						<?php else : ?>
 							<?php _e( 'Blog Archives', 'twentyeleven' ); ?>
 						<?php endif; ?>
