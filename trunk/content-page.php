a1f3239b01950bd309400924028716c1a8f921d7
raido357@gmail.com <raido357@gmail.com@d7ce5ed5-6a65-1e91-dfe9-0b47fc94c34d>
2012-08-07 09:25:25 +0000

    Page thumbnail size set to thumbnail

--- trunk/content-page.php
+++ trunk/content-page.php
@@ -41,7 +41,7 @@
 			<div class="entry-content">
 				<?php
 					if ( has_post_thumbnail() ) {
-						the_post_thumbnail();
+						the_post_thumbnail('medium');
 					} 
 				?>
 				<?php the_content(); ?>
