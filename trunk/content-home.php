cf5aa812e92f0b82ee37f86002a50d7b263900be
raido357@gmail.com <raido357@gmail.com@d7ce5ed5-6a65-1e91-dfe9-0b47fc94c34d>
2012-08-07 11:29:41 +0000

    Some fiddling with animations

--- trunk/content-home.php
+++ trunk/content-home.php
@@ -13,12 +13,31 @@
 <script type="text/javascript">
   jQuery(document).ready(function($){
   
-  	setTimeout(function() {
-		$('div.aim').find('a:nth-child(-n+4)').each(function() {
-			var old_bg = $(this).css('background-image');
-			$(this).css({'background' : 'url("wp-content/themes/hk-wp-mall/images/home-nav-shadow.png") right 35px no-repeat,'+ old_bg + 'no-repeat' });
+	$('div.aim').find('a:nth-child(-n+4)').each(function() {
+		var old_bg = $(this).css('background-image');
+		$(this).css({'background' : 'url("wp-content/themes/hk-wp-mall/images/home-nav-shadow.png") right 35px no-repeat,'+ old_bg + 'no-repeat' });
+		
+	});
+	
+	$('div.aim').find('a').each(function() {
+		var animationTimeout;
+		
+		var span = $(this).find('span:first');
+		$(this).hover(function() {
+			clearTimeout(animationTimeout);
+			span.animate({
+				marginLeft: '96px'
+			}, 400);
+		},
+		function() {
+			clearTimeout(animationTimeout);
+			animationTimeout = setTimeout(function() {
+				span.animate({
+					marginLeft: '-=96px'
+				}, 400);
+			}, 200);
 		});
-	}, 250 );
+	});
   
     $('#slider1').bxSlider({
     	displaySlideQty: 4,
