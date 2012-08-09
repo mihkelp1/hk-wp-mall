b908c7f5dbbf1182fa845ebee3e3633ef4247aa0
raido357@gmail.com <raido357@gmail.com@d7ce5ed5-6a65-1e91-dfe9-0b47fc94c34d>
2012-08-09 11:18:54 +0000

    Search form in header nav menu works, no need seperate styles for inline/in content displaying, 404 and search/search results pages

--- trunk/content-home.php
+++ trunk/content-home.php
@@ -34,36 +34,6 @@
 		});
 	});
 	
-	/* Search link */
-	
-	$('.header-menu-container li:nth-child(5)').bind( 'click', function(event) {
-		event.preventDefault();
-		event.stopPropagation();
-	});
-	
-	$('.header-menu-container li:nth-child(5)').hover(function() {
-		$('div.#rollout-searchform').animate({
-			'width' : 'show'
-		}, 'fast', function() {
-			$('div.#rollout-searchform').find('input:first').focus();
-		});
-	});
-	
-	$(document).live('click', function(e) {
-		var searchform = $('div.#rollout-searchform');
-		if ( e.target != searchform ) {
-			if ( searchform.is(':visible') ) {
-				$('div.#rollout-searchform').animate({
-					'width' : 'hide'
-				}, 'fast');
-			}
-		}
-	});
-	
-	$('div.#rollout-searchform').bind('click', function(e) {
-		e.stopPropagation();
-	});
-	$('div.#rollout-searchform').hide();
   
     $('#slider1').bxSlider({
     	displaySlideQty: 4,
