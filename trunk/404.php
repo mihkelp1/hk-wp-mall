b9274da33c084296af5174e5d4848951a25abd4f
raido357@gmail.com <raido357@gmail.com@d7ce5ed5-6a65-1e91-dfe9-0b47fc94c34d>
2012-08-09 08:45:15 +0000

    404 page styles

--- trunk/404.php
+++ trunk/404.php
@@ -17,30 +17,17 @@ get_header(); ?>
 	<div class="page-inside-content">
 		<article id="post-0" class="post error404 not-found">
 			<header class="entry-header">
-				<h1 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentyeleven' ); ?></h1>
+				<h1 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'hk-wp-mall' ); ?></h1>
 			</header>
 	
 			<div class="entry-content">
-				<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching, or one of the links below, can help.', 'twentyeleven' ); ?></p>
+				<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching, or one of the links, can help.', 'hk-wp-mall' ); ?></p>
 	
 				<?php get_search_form(); ?>
-	
-				<?php the_widget( 'WP_Widget_Recent_Posts', array( 'number' => 10 ), array( 'widget_id' => '404' ) ); ?>
-	
-				<div class="widget">
-					<h2 class="widgettitle"><?php _e( 'Most Used Categories', 'twentyeleven' ); ?></h2>
-					<ul>
-					<?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10 ) ); ?>
-					</ul>
+				<div class="widget-404">
+					<?php the_widget( 'WP_Widget_Recent_Posts', array( 'title' =>  __('Recent news', 'hk-wp-mall'), 'number' => 10 ), array('before_title' => '<h2 class="entry-title">', 'after_title' => '</h2>') ); ?>
+					<?php the_widget( 'WP_Widget_Tag_Cloud', array( 'title' =>  __('Tags', 'hk-wp-mall') ), array('before_title' => '<h2 class="entry-title">', 'after_title' => '</h2>' ) ); ?>
 				</div>
-	
-				<?php
-				/* translators: %1$s: smilie */
-				$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'twentyeleven' ), convert_smilies( ':)' ) ) . '</p>';
-				the_widget( 'WP_Widget_Archives', array('count' => 0 , 'dropdown' => 1 ), array( 'after_title' => '</h2>'.$archive_content ) );
-				?>
-	
-				<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
 	
 			</div><!-- .entry-content -->
 		</article><!-- #post-0 -->
