1bc2a45678ea0f5e6f7c9cfff7b9ed54daa3ed90
raido357@gmail.com <raido357@gmail.com@d7ce5ed5-6a65-1e91-dfe9-0b47fc94c34d>
2012-08-09 09:07:29 +0000

    Tagging version 0.2

---
+++ tags/0.2/404.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found).
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+get_header(); ?>
+
+<div class="main-content content-404">
+
+	<div class="page-left-menu">
+		<?php wp_nav_menu( array( 'theme_location' => 'last-resort-menu', 'container_class' => 'last-resort-menu-container' ) ); ?>
+	</div>
+	<div class="page-inside-content">
+		<article id="post-0" class="post error404 not-found">
+			<header class="entry-header">
+				<h1 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'hk-wp-mall' ); ?></h1>
+			</header>
+	
+			<div class="entry-content">
+				<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching, or one of the links, can help.', 'hk-wp-mall' ); ?></p>
+	
+				<?php get_search_form(); ?>
+				<div class="widget-404">
+					<?php the_widget( 'WP_Widget_Recent_Posts', array( 'title' =>  __('Recent news', 'hk-wp-mall'), 'number' => 10 ), array('before_title' => '<h2 class="entry-title">', 'after_title' => '</h2>') ); ?>
+					<?php the_widget( 'WP_Widget_Tag_Cloud', array( 'title' =>  __('Tags', 'hk-wp-mall') ), array('before_title' => '<h2 class="entry-title">', 'after_title' => '</h2>' ) ); ?>
+				</div>
+	
+			</div><!-- .entry-content -->
+		</article><!-- #post-0 -->
+	</div>
+
+</div><!-- #content -->
+
+<?php get_footer(); ?>
---
+++ tags/0.2/content-home.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * The default template for displaying content
+ */
+?>
+
+<div class="main-content content-home">
+	<?php echo do_shortcode('[a_image_menu]') ?>
+</div>
+
+<?php getNewsSlider(); ?>
+
+<script type="text/javascript">
+  jQuery(document).ready(function($){
+  
+	$('div.aim').find('a:nth-child(-n+4)').each(function() {
+		var old_bg = $(this).css('background-image');
+		$(this).css({'background' : 'url("wp-content/themes/hk-wp-mall/images/home-nav-shadow.png") right -10px no-repeat,'+ old_bg + 'no-repeat' });
+		
+	});
+	
+	$('div.aim').find('a').each(function() {
+		
+		var span = $(this).find('span:first');
+		$(this).hover(function(e) {
+			span.hoverFlow(e.type, {
+				marginLeft: '100px'
+			}, 400);
+		},
+		function(e) {
+			span.hoverFlow(e.type, {
+				marginLeft: '0px'
+			}, 375);
+		});
+	});
+  
+    $('#slider1').bxSlider({
+    	displaySlideQty: 4,
+	    moveSlideQty: 4,
+	    speed: 1000,
+	    onAfterSlide: function( currentSlideNumber, totalSlideQty, currentSlideHtmlObject ) {
+	    	var all_lis = currentSlideHtmlObject.parent().find('li').each(function() {
+	    		var li = $(this);
+	    		if ( li.hasClass( 'no-news-li-background' ) ) {
+	    			li.removeClass( 'no-news-li-background' );
+	    		}
+	    	});
+	    	var last_li = currentSlideHtmlObject.next().next().next();
+	    	last_li.addClass( 'no-news-li-background' );
+	    },
+	    onBeforeSlide: function( currentSlideNumber, totalSlideQty, currentSlideHtmlObject ) {
+	    	var all_lis = currentSlideHtmlObject.parent().find('li').each(function() {
+	    		var li = $(this);
+	    		if ( !li.hasClass( 'no-news-li-background' ) ) {
+	    			li.addClass( 'no-news-li-background' );
+	    		}
+	    	});
+	    }
+    });
+  });
+</script>
