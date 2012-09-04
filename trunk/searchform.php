12eacbbd95f2bb2d29820328a64fc07ea2bf17c1
raido357@gmail.com <raido357@gmail.com@d7ce5ed5-6a65-1e91-dfe9-0b47fc94c34d>
2012-09-04 07:13:56 +0000

    Testing fix for IE8

--- trunk/searchform.php
+++ trunk/searchform.php
@@ -8,8 +8,7 @@
  */
 ?>
 	<form method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-		<h1 style="color: #FFF">testing</h1>
-		<input type="text" tabindex="2" class="search-fieldff" name="s" autofocus="autofocus" placeholder="<?php esc_attr_e( 'Search', 'hk-wp-mall' ); ?>" />
+		<input type="text" tabindex="2" class="search-field" name="s" autofocus="autofocus" placeholder="<?php esc_attr_e( 'Search', 'hk-wp-mall' ); ?>" />
 	</form>
 <script type="text/javascript">	
 	jQuery(document).ready(function($){
