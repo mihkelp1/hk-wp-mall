022ecf0c8d19410da09cfb48e94cdf817c31b3f1
raido357@gmail.com <raido357@gmail.com@d7ce5ed5-6a65-1e91-dfe9-0b47fc94c34d>
2012-09-04 07:31:39 +0000

    Only select distinct emails from reminders table, so we would not send duplicate emails

--- trunk/class-reminders.php
+++ trunk/class-reminders.php
@@ -52,13 +52,13 @@ class HK_Reminders {
 		if ( !empty( $flag ) ) {
 			return $wpdb->get_var( $wpdb->prepare( 'SELECT count(*) FROM '.self::getTableName().' WHERE flag = %s', $flag ) );
 		} else {
-			return $wpdb->get_var( 'SELECT count(*) FROM '.self::getTableName() );
+			return $wpdb->get_var( 'SELECT DISTINCT count(*) FROM '.self::getTableName() );
 		}
 	}
 	
 	public static function getAll() {
 		global $wpdb;
-		return $wpdb->get_results( 'SELECT email FROM '.self::getTableName() );
+		return $wpdb->get_results( 'SELECT DISTINCT email FROM '.self::getTableName() );
 	}
 	
 	public static function addToHistory( $count = 0 ) {
