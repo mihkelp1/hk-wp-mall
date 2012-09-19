5a3318a8ef0fd094f27616f290d08ed515bd84cc
raido357@gmail.com <raido357@gmail.com@d7ce5ed5-6a65-1e91-dfe9-0b47fc94c34d>
2012-09-19 13:40:46 +0000

    Clean up, prepare for using language files, few todos left for tomorrow

--- archive.php
+++ archive.php
@@ -4,12 +4,6 @@
  *
  * Used to display archive-type pages if nothing more specific matches a query.
  * For example, puts together date-based pages if no date.php file exists.
- *
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Twenty_Eleven
- * @since Twenty Eleven 1.0
  */
 
 get_header(); ?>
@@ -29,9 +23,9 @@ get_header(); ?>
 						<?php if ( is_day() ) : ?>
 							<?php echo '<span>' . get_the_date() . '</span>'; ?>
 						<?php elseif ( is_month() ) : ?>
-							<?php echo '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'twentyeleven' ) ) . '</span>'; ?>
+							<?php echo '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'hk-wp-mall' ) ) . '</span>'; ?>
 						<?php elseif ( is_year() ) : ?>
-							<?php  echo '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'twentyeleven' ) ) . '</span>'; ?>
+							<?php  echo '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'hk-wp-mall' ) ) . '</span>'; ?>
 						<?php else : ?>
 							<?php
 								single_tag_title();
@@ -39,8 +33,6 @@ get_header(); ?>
 						<?php endif; ?>
 					</h1>
 				</header>
-
-				<?php //twentyeleven_content_nav( 'nav-above' ); ?>
 
 				<?php /* Start the Loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
@@ -55,17 +47,15 @@ get_header(); ?>
 
 				<?php endwhile; ?>
 
-				<?php //twentyeleven_content_nav( 'nav-below' ); ?>
-
 			<?php else : ?>
 
 				<article id="post-0" class="post no-results not-found">
 					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
+						<h1 class="entry-title"><?php _e( 'Nothing Found', 'hk-wp-mall' ); ?></h1>
 					</header><!-- .entry-header -->
 
 					<div class="entry-content">
-						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
+						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'hk-wp-mall' ); ?></p>
 						<?php get_search_form(); ?>
 					</div><!-- .entry-content -->
 				</article><!-- #post-0 -->
--- search.php
+++ search.php
@@ -44,11 +44,11 @@ get_header(); ?>
 
 			<article id="post-0" class="post no-results not-found">
 				<header class="entry-header">
-					<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
+					<h1 class="entry-title"><?php _e( 'Nothing Found', 'hk-wp-mall' ); ?></h1>
 				</header><!-- .entry-header -->
 
 				<div class="entry-content">
-					<p><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'twentyeleven' ); ?></p>
+					<p><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'hk-wp-mall' ); ?></p>
 					<?php get_search_form(); ?>
 				</div><!-- .entry-content -->
 			</article><!-- #post-0 -->
