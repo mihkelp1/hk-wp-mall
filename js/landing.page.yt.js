7603c84178031b30e8796e2a7667d9772ec0c20c
raido357@gmail.com <raido357@gmail.com@d7ce5ed5-6a65-1e91-dfe9-0b47fc94c34d>
2012-09-26 11:35:05 +0000

    Found a solution, by removing autohide=1

--- js/landing.page.yt.js
+++ js/landing.page.yt.js
@@ -122,7 +122,7 @@ jQuery(document).ready(function($) {
 
 function init_YT_Player() {
 	// IF IE7 or less, use Youtube JavaScript API
-	if ( !is_ie7 ) {
+	if ( is_ie7 ) {
 		var params = { allowScriptAccess: "always" };
 		var atts = { id: "yt_player" };
 		swfobject.embedSWF("http://www.youtube.com/v/"+landingPageMeta.videoId+"?enablejsapi=1&playerapiid=ytplayer&version=3&showinfo=0&modestbranding=1&rel=0&theme=light",
