6179df680b3a0bcefd9a55dbf25bbd3c0270362a
raido357@gmail.com <raido357@gmail.com@d7ce5ed5-6a65-1e91-dfe9-0b47fc94c34d>
2012-10-04 19:46:36 +0000

    Revert some args of pagination

--- branches/news-footer-right/trunk/functions.php
+++ branches/news-footer-right/trunk/functions.php
@@ -565,8 +565,6 @@ function getPaginationHTML() {
 	echo paginate_links( array(
 		'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
 		'format' => '?paged=%#%',
-		'end_size' => 2,
-		'mid_size' => 3,
 		'current' => max( 1, get_query_var('paged') ),
 		'total' => $wp_query->max_num_pages
 	) );
