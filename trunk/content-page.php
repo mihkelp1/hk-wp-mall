03b21d179195c76a95367b8574989bf6d34331cc
raido357@gmail.com <raido357@gmail.com@d7ce5ed5-6a65-1e91-dfe9-0b47fc94c34d>
2012-10-04 18:12:49 +0000

    Change post thumbnail size to medium

--- trunk/content-page.php
+++ trunk/content-page.php
@@ -54,7 +54,7 @@
 			<div class="entry-content">
 				<?php
 					if ( has_post_thumbnail() ) {
-						the_post_thumbnail('large');
+						the_post_thumbnail('medium');
 					} 
 				?>
 				<?php the_content(); ?>
