2ecbee84489a5f71c32b7da081f6629ade0449c7
raido357@gmail.com <raido357@gmail.com@d7ce5ed5-6a65-1e91-dfe9-0b47fc94c34d>
2012-10-05 09:39:31 +0000

    Add fallback_cb param = false

--- trunk/search.php
+++ trunk/search.php
@@ -11,7 +11,7 @@ get_header(); ?>
 
 <div class="main-content content-search">
 	<div class="page-left-menu">
-		<?php wp_nav_menu( array( 'theme_location' => 'last-resort-menu', 'container_class' => 'last-resort-menu-container' ) ); ?>
+		<?php wp_nav_menu( array( 'theme_location' => 'last-resort-menu', 'container_class' => 'last-resort-menu-container', 'fallback_cb' => false ) ); ?>
 	</div>
 	
 	<div class="page-inside-content">
