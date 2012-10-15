2f807bf2866ad135cc77a8d7ed87db3b6c377880
raido357@gmail.com <raido357@gmail.com@d7ce5ed5-6a65-1e91-dfe9-0b47fc94c34d>
2012-10-15 13:06:33 +0000

    Facebook button, search value kept in the field

--- trunk/searchform.php
+++ trunk/searchform.php
@@ -4,5 +4,5 @@
  */
 ?>
 	<form method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-		<input type="text" tabindex="2" class="search-field" name="s" autofocus="autofocus" placeholder="<?php esc_attr_e( 'Search', 'hk-wp-mall' ); ?>" />
+		<input type="text" tabindex="2" value="<?php the_search_query(); ?>" class="search-field" name="s" autofocus="autofocus" placeholder="<?php esc_attr_e( 'Search', 'hk-wp-mall' ); ?>" />
 	</form>
